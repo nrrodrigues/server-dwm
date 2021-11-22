feb64a12c546e543919bc63e0c0e8922e2221ba9
Nuno Rodrigues <nuno.rodrigues@jci.com>
2021-11-22 10:23:02 +0000

    change sequelize options

--- app/models/index.js
+++ app/models/index.js
@@ -2,6 +2,13 @@ const Sequelize = require("sequelize");
 const dbConnectionString = process.env.DATABASE_URL || "postgres://postgres:123@localhost:5432/testdb"
 const sequelize = new Sequelize(dbConnectionString, {
   dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
+  native: true,
   pool: {
     max: 5,
     min: 0,
